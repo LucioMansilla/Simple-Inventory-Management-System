6d0d1e9c2fadbd422bd3b9754527a8ecc14bd40b
LucioMansilla <luciomansillaztw@gmail.com>
2024-03-28 14:56:40 -0300

    Implement Product class with properties and constructor

---
+++ Simple_Inventory_Management_System/Product.cs
@@ -0,0 +1,22 @@
+namespace Simple_Inventory_Management_System;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    public Product(string name, decimal price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Price: {Price:C}, Quantity: {Quantity}";
+    }
+    
+}
